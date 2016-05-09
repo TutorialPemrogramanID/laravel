c6658cbe8b899101dd695b9fd2b1513cb67bcf2c
hendrikusR <hendrikus.richardo@gmail.com>
2016-05-10 00:24:00 +0700

    first commit

---
+++ database/migrations/2016_05_09_165412_buat_table_anggota.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class BuatTableAnggota extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kategori', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nama_kategori');
+            $table->string('icon');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('kategori');
+    }
+}
